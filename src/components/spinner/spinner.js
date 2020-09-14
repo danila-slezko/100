e491f0ee193c41d0b2b1a9cd013c58e362a5adb6
danila slezko <danila.slezko@cbsinteractive.com>
2020-09-14 11:45:52 +0300

    with error
    with error

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-gnfi5cwe5ru"><div className="ldio-08n3s7pucex">
+<div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+</div></div>
+    )
+    
+}
+
+export default Spinner;
